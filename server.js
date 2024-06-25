8c73c03d263f4c13f0af0c181fe3d89e61c2afb2
Gaurav1801 <gaurav.rt18@gmail.com>
2024-06-25 17:16:18 +0530

    the env file made and mongoDb server swift to online mode

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const db = require('./db')
+require('dotenv').config(); // allows to use the env file
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json()); //req.body
@@ -23,6 +24,6 @@ const menuRoutes = require('./routes/menuRoutes');
 app.use('/person', personRoutes)
 app.use('/menu', menuRoutes)
 
+const PORT = process.env.PORT || 3000; // process.env.var_name is use to access the variable
 
-
-app.listen(3000, () => { console.log("server is runing on 3000") })
+app.listen(PORT, () => { console.log("server is runing on 3000") })
